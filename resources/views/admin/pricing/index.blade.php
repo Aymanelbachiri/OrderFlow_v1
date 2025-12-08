100d6d5e04fc442a4ce0eb7885be62446df3515a
Aymanelbachiri <ayman.elbachiri666@gmail.com>
2025-12-09 00:44:48 +0100

    pricing view goes to checkout page

--- resources/views/admin/pricing/index.blade.php
+++ resources/views/admin/pricing/index.blade.php
@@ -108,8 +108,9 @@
 
                                             <div class="flex space-x-2">
 
-                                                <a href="{{ route('admin.pricing.show', $plan) }}" 
-                                                   class="flex-1 bg-blue-500 hover:from-blue-600/90 hover:to-blue-600/70 text-white text-center py-2 px-3 rounded-lg text-sm font-semibold transition-all duration-300 flex items-center justify-center space-x-1">
+                                                <a href="{{ route('checkout.show', ['plan_id' => $plan->id, 'source' => 'admin']) }}"
+                                                   target="_blank"
+                                                   class="flex-1 bg-blue-500 hover:bg-blue-600 text-white text-center py-2 px-3 rounded-lg text-sm font-semibold transition-all duration-300 flex items-center justify-center space-x-1">
                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                                     <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
                                                     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
